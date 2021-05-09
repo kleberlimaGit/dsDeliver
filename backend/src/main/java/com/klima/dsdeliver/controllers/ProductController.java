b9c31661915d5b811e68e49869f186832b75dbec
kleberlimagit <mov_kleber@hotmail.com>
2021-05-09 01:09:10 -0300

    Crud de Produtos

--- backend/src/main/java/com/klima/dsdeliver/controllers/ProductController.java
+++ backend/src/main/java/com/klima/dsdeliver/controllers/ProductController.java
@@ -1,12 +1,19 @@
 package com.klima.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.klima.dsdeliver.dto.ProductDTO;
 import com.klima.dsdeliver.service.ProductService;
@@ -25,5 +32,29 @@ public class ProductController {
 		List<ProductDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public  ResponseEntity<ProductDTO> insertProduct(@RequestBody ProductDTO dto){
+		dto = service.insertProduct(dto);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto); 
+		
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id,@RequestBody ProductDTO dto){
+		dto = service.updateProduct(id, dto);
+		return ResponseEntity.ok().body(dto);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long id){
+		service.deleteProduct(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/klima/dsdeliver/service/ProductService.java
+++ backend/src/main/java/com/klima/dsdeliver/service/ProductService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.ResourceAccessException;
 
 import com.klima.dsdeliver.dto.ProductDTO;
 import com.klima.dsdeliver.entities.Product;
@@ -26,5 +29,38 @@ public class ProductService {
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
 	
+	public ProductDTO insertProduct(ProductDTO dto) {
+		Product product = new Product(null,dto.getName(),dto.getPrice(),dto.getDescription(),dto.getImageUri());
+		product = repository.save(product);
+		return new ProductDTO(product);
+	}
+	
+	public ProductDTO updateProduct(Long id, ProductDTO dto) {
+		try {
+			Product product = repository.getOne(id);	
+			product.setDescription(dto.getDescription());
+			product.setImageUri(dto.getImageUri());
+			product.setName(dto.getName());
+			product.setPrice(dto.getPrice());
+			product = repository.save(product);
+			return new ProductDTO(product);
+			
+		}
+		catch (RuntimeException e) {
+			throw new ResourceAccessException("identificador não encontrado. " + id);
+		}
+	}
+	
+	public void deleteProduct(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceAccessException("identificador não encontrado. " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Violação de integridade");
+		}
+	}
 	
 }
